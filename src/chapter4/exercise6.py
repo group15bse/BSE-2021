5fc58925233cfd7ca6a91d4d3a4ec13e88f0b22d
group5bse1 <group5bse1@gmail.com>
2021-04-18 17:06:12 +0300

    assignment 4

---
+++ src/chapter4/exercise6.py
@@ -0,0 +1,11 @@
+def computepay():
+    hours = float(input("Enter hours:"))
+    rate = float(input("Enter rate:"))
+    if hours > 40:
+        epay = (hours - 40) * rate * 1.5
+        pay = (40 * rate) + epay
+    else:
+        pay = hours * rate
+    print("Pay",pay)
+computepay()
+
