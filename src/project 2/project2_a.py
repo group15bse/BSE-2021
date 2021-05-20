a3593f99ea157d569e38e029c447ef539da35224
group15bse1 <group15bse1@gmail.com>
2021-05-20 12:40:08 +0300

    first version of the program

--- src/project 2/project2_a.py
+++ src/project 2/project2_a.py
@@ -1,6 +1,16 @@
-xfile=open("measles.txt","r")
-count = 0
-for line in xfile:
-    count = count + 1
-print('Line Count:', count)
+handle = open('measles.txt','r')
+inp = input('Enter a year: ')
+output = input('Enter a file name for output: ')
+if not '.txt' in output:
+    print('File name should be in txt format with .txt ext.')
+    exit()
+save = open(output,'w')
+for line in handle:
+    if inp in line[88:92]:
+        if line[88:92].startswith(inp):
+            save.write(line)
+    else:
+        if inp =='' or inp == 'all' or inp =='ALL':
+            save.write(line)
+handle.close()
 
