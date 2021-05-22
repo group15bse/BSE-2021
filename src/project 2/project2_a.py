ab2b85fea5e06d4625ec36f0836c3cb22e74e507
group15bse1 <group15bse1@gmail.com>
2021-05-22 15:39:28 +0300

    project 2

--- src/project 2/project2_a.py
+++ src/project 2/project2_a.py
@@ -1,16 +1,32 @@
-handle = open('measles.txt','r')
-inp = input('Enter a year: ')
-output = input('Enter a file name for output: ')
-if not '.txt' in output:
-    print('File name should be in txt format with .txt ext.')
-    exit()
-save = open(output,'w')
-for line in handle:
-    if inp in line[88:92]:
-        if line[88:92].startswith(inp):
-            save.write(line)
+try:
+    handle = open('measles.txt','r')
+    inp = input('Enter a year: ')
+    if inp == '' or inp == 'all' or inp == 'ALL':
+        pass
+    elif int(inp):
+        if len(inp) <= 4:
+           pass
     else:
-        if inp =='' or inp == 'all' or inp =='ALL':
-            save.write(line)
-handle.close()
+        print('Invalid input!!!')
 
+    output = input('Enter a file name for output: ')
+
+    if not '.txt' in output:
+        print('File name should be in txt format with .txt ext.')
+        exit()
+    save = open(output,'w')
+
+    for line in handle:
+        if inp in line[88:92]:
+            if line[88:92].startswith(inp):
+                save.write(line)
+        #else:
+           #if inp =='' or inp == 'all' or inp =='ALL':
+               # save.write(line)
+
+
+    save.close()
+    handle.close()
+
+except:
+    print('Error....,Invalid input!!!!')
